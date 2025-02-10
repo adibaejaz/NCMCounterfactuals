2672ecab92013534c14a048890fbee7fd947b530
adibaejaz <adibaejaz@gmail.com>
2025-02-10 11:29:37 -0500

    parallelised run_score function

--- example3.py
+++ example3.py
@@ -10,6 +10,7 @@ import sys
 import warnings
 import argparse
 from src.ds import CTF
+import multiprocessing
 
 import numpy as np
 import torch as T
@@ -21,6 +22,28 @@ from src.scm.ncm import FF_NCM, GAN_NCM, MLE_NCM
 from src.run import NCMRunner, NCMMinMaxRunner
 from src.ds.causal_graph import CausalGraph
 from src.metric.queries import get_query, get_experimental_variables, is_q_id_in_G
+
+
+
+
+def run_graph(true_graph, gpu_id, args, pipeline, dat_model, ncm_model, test_graph_files, n, d, hyperparams, original_stdout):
+    gen_cg_file = "dat/cg/{}.cg".format(true_graph)
+    with open("output_ground_truth_{}".format(true_graph), 'w') as f:
+        sys.stdout = f
+        print("---- DATA GENERATED FROM ---- ", true_graph)
+        for i in range(args.n_trials):
+            print("ON TRIAL", i)
+            runner = NCMRunner(pipeline, dat_model, ncm_model)
+            try:
+                runner.run_score(args.name, gen_cg_file, test_graph_files, n, d, i,
+                             hyperparams=hyperparams, gpu=[gpu_id], verbose=args.verbose)
+            except Exception as e:
+                print(e, "ERROR whlie processing ground truth", true_graph, "trial", i)
+        sys.stdout = original_stdout
+
+
+
+
 
 os.environ['CUBLAS_WORKSPACE_CONFIG'] = ':4096:8'
 
@@ -145,7 +168,7 @@ dat_model = valid_generators[gen_choice]
 ncm_model = architectures[pipeline_choice]
 
 #gpu_used = 0 if args.gpu is None else [int(args.gpu)]
-gpu_used = [0]
+gpu_used = [0,1,2,3,4,5,6]
 
 
 arg_data_bs = args.data_bs
@@ -253,29 +276,30 @@ for graph in graph_set:
 
         original_stdout = sys.stdout
 
-        print("THREE NODE GRAPHS")
-        for tn_graph in three_node_graphs:
-            gen_cg_file = "dat/cg/{}.cg".format(tn_graph)
-            with open("output_ground_truth_{}".format(tn_graph), 'w') as f:
-                sys.stdout = f
-                print("---- DATA GENERATED FROM ---- ", tn_graph)
-                for i in range(args.n_trials):
-                    print("ON TRIAL", i)
-                    runner = NCMRunner(pipeline, dat_model, ncm_model)
-                    runner.run_score(args.name, gen_cg_file, three_node_files, n, d, i,
-                                    hyperparams=hyperparams, gpu=gpu_used, verbose=args.verbose)
-                sys.stdout = original_stdout
+        num_gpus = T.cuda.device_count()
 
+        print("THREE NODE GRAPHS")
+        processes = []
+        for idx, tn_graph in enumerate(three_node_graphs):
+            gpu_id = idx % num_gpus  # Assign GPU in a round-robin manner
+            p = multiprocessing.Process(target=run_graph, args=(tn_graph, gpu_id, args, pipeline, dat_model, ncm_model, 
+                                                                three_node_files, n, d, hyperparams, original_stdout))
+            processes.append(p)
+            p.start()
+
+        for p in processes:
+            p.join() 
+
+        
         print("FOUR NODE GRAPHS")
-        for fn_graph in four_node_graphs:
-            gen_cg_file = "dat/cg/{}.cg".format(fn_graph)
-            with open("output_ground_truth_{}.txt".format(fn_graph), 'w') as f:
-                sys.stdout = f
-                print("---- DATA GENERATED FROM ---- ",fn_graph)
-                for i in range(args.n_trials):
-                    print("ON TRIAL", i)
-                    print("---- DATA GENERATED FROM ---- ", gen_cg_file)
-                    runner = NCMRunner(pipeline, dat_model, ncm_model)
-                    runner.run_score(args.name, gen_cg_file, four_node_files, n, d, i,
-                                    hyperparams=hyperparams, gpu=gpu_used, verbose=args.verbose)
-                sys.stdout = original_stdout
+        processes = []
+        for idx, tn_graph in enumerate(four_node_graphs):
+            gpu_id = idx % num_gpus  # Assign GPU in a round-robin manner
+            p = multiprocessing.Process(target=run_graph, args=(tn_graph, gpu_id, args, pipeline, dat_model, ncm_model, 
+                                                                four_node_files, n, d, hyperparams, original_stdout))
+            processes.append(p)
+            p.start()
+
+        for p in processes:
+            p.join()
+
